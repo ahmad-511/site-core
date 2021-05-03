6248cc8a1ecdd653fec733ba553b1c89a3c3c355
Ahmad Murey <ahmad.murey@gmail.com>
2021-05-03 15:43:09 +0300

    BuildSQLFilter / Uppercasing criteria operators

--- App/Core/DB.php
+++ App/Core/DB.php
@@ -218,7 +218,6 @@ class DB {
         $sqlFilter = [];
         $sqlParams = [];
         
-        $whereClause = 'WHERE';
         $operator = '';
 
         foreach($criteria as $pn => $crt){
@@ -256,7 +255,7 @@ class DB {
             }
             
             if(!empty($condition)){
-                $sqlFilter[] = (empty($sqlFilter)?$whereClause: $operator).' '.$condition;
+                $sqlFilter[] = (empty($sqlFilter)?'WHERE': strtoupper($operator)).' '.$condition;
             }
         }
 
